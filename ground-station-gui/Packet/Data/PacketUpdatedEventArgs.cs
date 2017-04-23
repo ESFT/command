fee2c765cd9dcad3df59885607692215c44407f4
Ryan William Claus <ryan.w.claus@gmail.com>
2017-04-22 18:14:28 -0700

    Remove files to setup new system

--- ground-station-gui/Packet/Data/PacketUpdatedEventArgs.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace ESFT.GroundStation.Packet.Data {
-    internal class PacketUpdatedEventArgs : EventArgs {
-        public DateTime UpdateTime { get; set; }
-        public IPacket Packet { get; set; }
-    }
-}
